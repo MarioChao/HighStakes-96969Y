74ec0bf56a2a1947acf74cdc0dca7dc3702badcc
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-10-24 00:46:10 +0800

    auton red down 10/23

--- src/Autonomous/Paths/redDown.cpp
+++ src/Autonomous/Paths/redDown.cpp
@@ -5,54 +5,44 @@ void autonpaths::runAutonRedDown() {
 	timer autontimer;
 	setRotation(-90.0);
 
-
 	/* Start facing left */
 
 	// Grab middle goal
 	turnToAngle(-90.0);
-	driveAndTurnDistanceTiles(-1.5, -90.0, 60);
-	turnToAngle(-118.0, -halfRobotLengthIn * 0.5);
-	setGoalClampState(1, 0.92);
-	driveAndTurnDistanceTiles(-0.7, -118.0, 30.0, 100.0, defaultMoveTilesErrorRange, 3.0);
+	driveAndTurnDistanceTiles(-1.35, -90.0, 100);
+	turnToAngle(-118.0, halfRobotLengthIn * 0.5);
+	setGoalClampState(1, 0.7);
+	driveAndTurnDistanceTiles(-0.73, -118.0, 60.0, 100.0, defaultMoveTilesErrorRange, 2.0);
 
+	// Score 1 ring
 	task::sleep(200);
 	setIntakeState(1);
-	// Score 1 ring
-	//turnToAngle(125.0);
-	task::sleep(200);
-	driveAndTurnDistanceTiles(0.25, -118.0, 40.0);
+	task::sleep(700);
 
-	task::sleep(500);
-
-
-	// Drop goal
-	//setIntakeState(0);
-	turnToAngle(-50);
+	// Drop goal & face bottom ring
+	turnToAngleVelocity(-50, 50.0, halfRobotLengthIn * 0.85);
 	setGoalClampState(0);
+	setIntakeState(0);
 
 	// Intake bottom ring
 	setIntakeTopState(0);
 	setIntakeBottomState(1);
-	driveAndTurnDistanceTiles(0.6, -50, 20.0, 100.0);
-
+	setIntakeBottomState(0, 0.5);
+	driveAndTurnDistanceTiles(0.5, -50, 60.0, 100.0);
 
 	// Grab bottom goal
 	turnToAngle(-180.0);
-	driveAndTurnDistanceTiles(-0.45, -180.0);
-	driveAndTurnDistanceTiles(-0.37, -180.0, 20.0);
+	driveAndTurnDistanceTiles(-0.75, -180.0, 30.0);
 	setGoalClampState(1);
-
 
 	// Score 1 ring
 	setIntakeState(1);
 	task::sleep(750);
 
-
 	// Drop goal
 	turnToAngle(-270.0);
 	setIntakeState(0);
 	setGoalClampState(0);
-
 
 	// Touch the ladder
 	driveAndTurnDistanceTiles(0.9, -270.0, 30.0, 100.0);
