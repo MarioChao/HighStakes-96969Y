9897d8aa2dab2a027399b3ada93e774308179151
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-11-16 17:16:32 +0800

    filter pneumatic

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -50,7 +50,7 @@ pneumatics IntakeLiftPneumatic(Expander1.B);
 pneumatics HangPneumatic(Expander1.E);
 pneumatics GoalClampPneumatic(Brain.ThreeWirePort.C);
 pneumatics BotArmPneumatics(Expander1.B);
-pneumatics SwordPneumatics(Expander1.C);
+pneumatics SwordPneumatics(Brain.ThreeWirePort.D);
 
 
 // Sensors
