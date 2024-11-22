ad450dd3d8baace20621b3c5e0c344ddd7775fea
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-11-22 17:01:35 +0800

    added cubic splines and matrices

---
+++ include/GraphUtilities/matrix.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Matrix {
+public:
+	Matrix(int d1, int d2);
+	Matrix(std::vector<std::vector<double>> matrix_data);
+
+	Matrix multiply(Matrix &other);
+	bool canMultiply(Matrix &other);
+
+	Matrix operator*(double s);
+	Matrix &operator*=(double s);
+
+	std::pair<int, int> getShape();
+
+	std::string getString();
+
+	std::vector<std::vector<double>> data;
+
+private:
+	std::pair<int, int> shape;
+};
