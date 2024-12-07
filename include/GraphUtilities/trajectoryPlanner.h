dfe4998d3f9d43415e59316d6a4744a05ff8c948
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-12-07 18:06:55 +0800

    finished Trajectory Planner

--- include/GraphUtilities/trajectoryPlanner.h
+++ include/GraphUtilities/trajectoryPlanner.h
@@ -3,9 +3,16 @@
 #include <vector>
 #include <algorithm>
 
+namespace trajectory {
+	using merged_kinematics = std::vector<std::pair<double, std::pair<
+		std::pair<bool, std::vector<double>>,
+		std::pair<bool, std::vector<double>>
+	>>>;
+}
+
 class TrajectoryPlanner {
 public:
-	TrajectoryPlanner(double totalDistance);
+	TrajectoryPlanner(double totalDistance = 0);
 
 	void _onInit(double totalDistance);
 
@@ -17,7 +24,7 @@ public:
 
 	std::vector<std::pair<double, std::vector<double>>> _getForwardKinematics();
 	std::vector<std::pair<double, std::vector<double>>> _getBackwardKinematics();
-	std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>>>> _getMergedForwardBackward();
+	trajectory::merged_kinematics _getMergedForwardBackward();
 	std::vector<std::pair<double, std::vector<double>>> _getCombinedKinematics();
 	void calculateMotion();
 
--- src/GraphUtilities/trajectoryPlanner.cpp
+++ src/GraphUtilities/trajectoryPlanner.cpp
@@ -1,5 +1,7 @@
 #include "GraphUtilities/trajectoryPlanner.h"
 
+#include <stdio.h>
+
 TrajectoryPlanner::TrajectoryPlanner(double totalDistance) {
 	_onInit(totalDistance);
 }
@@ -13,10 +15,7 @@ void TrajectoryPlanner::addDesiredMotionConstraints(
 	double startDistance, double maxVelocity,
 	double maxAccel, double maxDecel
 ) {
-	std::pair<double, std::vector<double>> constraint = std::make_pair(
-		totalDistance,
-		std::vector<double>({maxVelocity, maxAccel, maxDecel})
-	);
+	std::pair<double, std::vector<double>> constraint = {startDistance, {maxVelocity, maxAccel, maxDecel}};
 	distance_motionConstraints.push_back(constraint);
 }
 
@@ -39,10 +38,8 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getForwa
 
 		// Push increasing-velocity kinematics info
 		if (travellingVelocity < maxVelocity) {
-			distance_kinematics.push_back(std::make_pair(
-				distanceStart,
-				std::vector<double>({travellingVelocity, maxAccel})
-			));
+			distance_kinematics.push_back({distanceStart, {travellingVelocity, maxAccel}});
+			// printf("Push: %.3f, %.3f, %.3f\n", distanceStart, travellingVelocity, maxAccel);
 		}
 
 		// Update velocity using constraint
@@ -56,10 +53,8 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getForwa
 
 		// Push constant-velocity kinematics info
 		if (maxVelDistance < segmentDistance) {
-			distance_kinematics.push_back(std::make_pair(
-				distanceStart + maxVelDistance,
-				std::vector<double>({maxVelocity, 0})
-			));
+			distance_kinematics.push_back({distanceStart + maxVelDistance, {maxVelocity, 0}});
+			// printf("Push max: %.3f, %.3f, 0\n", distanceStart + maxVelDistance, maxVelocity);
 		}
 
 		// Update velocity using acceleration
@@ -82,7 +77,7 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getBackw
 	double travellingVelocity = 0;
 	for (int segment = segmentCount - 1; segment >= 0; segment--) {
 		// Get segment info for [segment, segment + 1]
-		const double distanceStart = (segment == segmentCount - 1) ? 0 : totalDistance - distance_motionConstraints[segment].first;
+		const double distanceStart = (segment == segmentCount - 1) ? 0 : totalDistance - distance_motionConstraints[segment + 1].first;
 		const double distanceEnd = totalDistance - distance_motionConstraints[segment].first;
 		const double segmentDistance = distanceEnd - distanceStart;
 		std::vector<double> &motionConstraints = distance_motionConstraints[segment].second;
@@ -92,10 +87,8 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getBackw
 
 		// Push increasing-velocity kinematics info
 		if (travellingVelocity < maxVelocity) {
-			distance_kinematics.push_back(std::make_pair(
-				distanceStart,
-				std::vector<double>({travellingVelocity, maxAccel})
-			));
+			distance_kinematics.push_back({distanceStart, {travellingVelocity, maxAccel}});
+			// printf("Push: %.3f, %.3f, %.3f\n", distanceStart, travellingVelocity, maxAccel);
 		}
 
 		// Update velocity using constraint
@@ -109,10 +102,8 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getBackw
 
 		// Push constant-velocity kinematics info
 		if (maxVelDistance < segmentDistance) {
-			distance_kinematics.push_back(std::make_pair(
-				distanceStart + maxVelDistance,
-				std::vector<double>({maxVelocity, 0})
-			));
+			distance_kinematics.push_back({distanceStart + maxVelDistance, {maxVelocity, 0}});
+			// printf("Push max: %.3f, %.3f, 0\n", distanceStart + maxVelDistance, maxVelocity);
 		}
 
 		// Update velocity using acceleration
@@ -126,7 +117,13 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getBackw
 	for (int i = 0; i < (int) distance_kinematics.size(); i++) {
 		// Swap segment endpoint & reverse the distance
 		double distanceEnd = (i == (int) distance_kinematics.size() - 1) ? totalDistance : distance_kinematics[i + 1].first;
+		const double segmentDistance = distanceEnd - distance_kinematics[i].first;
 		distance_kinematics[i].first = totalDistance - distanceEnd;
+
+		// Calculate flipped velocity
+		const double v = distance_kinematics[i].second[0];
+		const double a = distance_kinematics[i].second[1];
+		distance_kinematics[i].second[0] = std::sqrt(std::pow(v, 2) + 2 * a * segmentDistance);
 
 		// Negate acceleration
 		distance_kinematics[i].second[1] *= -1;
@@ -137,7 +134,7 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getBackw
 	return distance_kinematics;
 }
 
-std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>>>> TrajectoryPlanner::_getMergedForwardBackward() {
+trajectory::merged_kinematics TrajectoryPlanner::_getMergedForwardBackward() {
 	// Forward kinematics
 	std::vector<std::pair<double, std::vector<double>>> forward_distance_kinematics = _getForwardKinematics();
 
@@ -146,29 +143,36 @@ std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>
 
 	// Size info
 	const int forward_size = (int) forward_distance_kinematics.size();
-	const int backward_size = (int) forward_distance_kinematics.size();
+	const int backward_size = (int) backward_distance_kinematics.size();
+
+	// for (int i = 0; i < forward_size; i++) {
+	// 	printf("%.3f, %.3f, %.3f\n", forward_distance_kinematics[i].first, forward_distance_kinematics[i].second[0], forward_distance_kinematics[i].second[1]);
+	// }
+	// for (int i = 0; i < backward_size; i++) {
+	// 	printf("%.3f, %.3f, %.3f\n", backward_distance_kinematics[i].first, backward_distance_kinematics[i].second[0], backward_distance_kinematics[i].second[1]);
+	// }
 
 	// Merge forward and backward kinematics
-	std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>>>> bothside_distance_kinematics = {};
+	trajectory::merged_kinematics bothside_distance_kinematics = {};
 	int forward_index, backward_index;
 	forward_index = backward_index = 0;
-	while (forward_index < forward_size && backward_index <= backward_size) {
+	while (forward_index < forward_size && backward_index < backward_size) {
 		// Get info
 		const double forward_distance = forward_distance_kinematics[forward_index].first;
 		const std::vector<double> &forward_kinematics = forward_distance_kinematics[forward_index].second;
-		const double backward_distance = forward_distance_kinematics[backward_index].first;
+		const double backward_distance = backward_distance_kinematics[backward_index].first;
 		const std::vector<double> &backward_kinematics = backward_distance_kinematics[backward_index].second;
 
 		// Push smaller distance
-		if (std::fabs(forward_distance - backward_distance) < 1e7) {
-			bothside_distance_kinematics.push_back({forward_distance, {forward_kinematics, backward_kinematics}});
+		if (std::fabs(forward_distance - backward_distance) < 1e-7) {
+			bothside_distance_kinematics.push_back({forward_distance, {{1, forward_kinematics}, {1, backward_kinematics}}});
 			forward_index++;
 			backward_index++;
 		} else if (forward_distance < backward_distance) {
-			bothside_distance_kinematics.push_back({forward_distance, {forward_kinematics, {0, 0}}});
+			bothside_distance_kinematics.push_back({forward_distance, {{1, forward_kinematics}, {0, {0, 0}}}});
 			forward_index++;
 		} else {
-			bothside_distance_kinematics.push_back({backward_distance, {{0, 0}, backward_kinematics}});
+			bothside_distance_kinematics.push_back({backward_distance, {{0, {0, 0}}, {1, backward_kinematics}}});
 			backward_index++;
 		}
 	}
@@ -178,16 +182,16 @@ std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>
 		const std::vector<double> &forward_kinematics = forward_distance_kinematics[forward_index].second;
 
 		// Push
-		bothside_distance_kinematics.push_back({forward_distance, {forward_kinematics, {0, 0}}});
+		bothside_distance_kinematics.push_back({forward_distance, {{1, forward_kinematics}, {0, {0, 0}}}});
 		forward_index++;
 	}
 	while (backward_index < backward_size) {
 		// Get info
-		const double backward_distance = forward_distance_kinematics[backward_index].first;
+		const double backward_distance = backward_distance_kinematics[backward_index].first;
 		const std::vector<double> &backward_kinematics = backward_distance_kinematics[backward_index].second;
 
 		// Push
-		bothside_distance_kinematics.push_back({backward_distance, {{0, 0}, backward_kinematics}});
+		bothside_distance_kinematics.push_back({backward_distance, {{0, {0, 0}}, {1, backward_kinematics}}});
 		backward_index++;
 	}
 
@@ -196,13 +200,91 @@ std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>
 }
 
 std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getCombinedKinematics() {
-	// Get merged forward and backward
-	std::vector<std::pair<double, std::pair<std::vector<double>, std::vector<double>>>> merged_distance_kinematics = _getMergedForwardBackward();
+	// Get merged forward and backward kinematics
+	trajectory::merged_kinematics merged_distance_kinematics = _getMergedForwardBackward();
 
-	// TODO: Get minimized kinematics
+	// for (int i = 0; i < (int) merged_distance_kinematics.size(); i++) {
+	// 	auto &distance_kinematics = merged_distance_kinematics[i];
+	// 	printf("%.3f, %d, %d\n", distance_kinematics.first, distance_kinematics.second.first.first, distance_kinematics.second.second.first);
+	// }
+
+	// Initialize forward and backward travelling kinematics
+	std::vector<double> forwardTravellingKinematics, backwardTravellingKinematics;
+	forwardTravellingKinematics = backwardTravellingKinematics = {0, 0};
+	double lastDistance = 0;
+	double lastIntersectionDistance = -1;
+
+	// Initialize result
 	std::vector<std::pair<double, std::vector<double>>> combined_distance_kinematics = {};
-	std::vector<double> forwardTravellingMotion, backwardTravellingMotion;
-	forwardTravellingMotion = backwardTravellingMotion = {0, 0, 0};
+
+	// Get minimized kinematics
+	for (auto &distance_kinematics : merged_distance_kinematics) {
+		// printf("K: %.3f, %d, %d\n", distance_kinematics.first, distance_kinematics.second.first.first, distance_kinematics.second.second.first);
+
+		// Check if intersection occured before distance
+		const double distanceStart = distance_kinematics.first;
+		if (lastDistance < lastIntersectionDistance && lastIntersectionDistance < distanceStart) {
+			// Push backward kinematics at intersection
+			combined_distance_kinematics.push_back({lastIntersectionDistance, backwardTravellingKinematics});
+		}
+
+		// Calculate travel distance
+		const double travelDistance = distanceStart - lastDistance;
+		lastDistance = distanceStart;
+
+		// Update travelling kinematics
+		const auto &_forwardKinematics = distance_kinematics.second.first;
+		const auto &_backwardKinematics = distance_kinematics.second.second;
+		if (_forwardKinematics.first) {
+			forwardTravellingKinematics = _forwardKinematics.second;
+		} else {
+			forwardTravellingKinematics[0] = std::sqrt(
+				std::pow(forwardTravellingKinematics[0], 2)
+				+ 2 * forwardTravellingKinematics[1] * travelDistance
+			);
+		}
+		if (_backwardKinematics.first) {
+			backwardTravellingKinematics = _backwardKinematics.second;
+		} else {
+			backwardTravellingKinematics[0] = std::sqrt(
+				std::pow(backwardTravellingKinematics[0], 2)
+				+ 2 * backwardTravellingKinematics[1] * travelDistance
+			);
+		}
+
+		// Aliases for travelling kinematics
+		auto &v = forwardTravellingKinematics[0];
+		auto &a = forwardTravellingKinematics[1];
+		auto &u = backwardTravellingKinematics[0];
+		auto &b = backwardTravellingKinematics[1];
+
+		// Push new kinematics info
+		if (combined_distance_kinematics.empty()) {
+			combined_distance_kinematics.push_back({distanceStart, {forwardTravellingKinematics}});
+			// printf("1st: %.3f, %.3f\n", distanceStart, forwardTravellingKinematics[0]);
+		} else {
+			// printf("start1: %.3f, %.3f, %.3f\n", distanceStart, forwardTravellingKinematics[0], forwardTravellingKinematics[1]);
+			// printf("start2: %.3f, %.3f, %.3f\n", distanceStart, backwardTravellingKinematics[0], backwardTravellingKinematics[1]);
+			if (v < u) {
+				combined_distance_kinematics.push_back({distanceStart, {forwardTravellingKinematics}});
+				// printf("chose 1\n");
+			} else {
+				combined_distance_kinematics.push_back({distanceStart, {backwardTravellingKinematics}});
+				// printf("chose 2\n");
+			}
+		}
+
+		// Calculate new intersection (if it exists)
+		bool hasNewIntersection = !(forwardTravellingKinematics[1] == 0 && backwardTravellingKinematics[1] == 0);
+		if (hasNewIntersection) {
+			// w^2 = vi^2 + 2aΔs = ui^2 + 2bΔs
+			// vi^2 + 2aΔs = ui^2 + 2bΔs
+			// Δs = (ui^2 - vi^2) / (2a - 2b)
+			lastIntersectionDistance = distanceStart + (std::pow(u, 2) - std::pow(v, 2)) / (2 * a - 2 * b);
+		} else {
+			lastIntersectionDistance = -1;
+		}
+	}
 
 	// Return result
 	return combined_distance_kinematics;
@@ -210,13 +292,51 @@ std::vector<std::pair<double, std::vector<double>>> TrajectoryPlanner::_getCombi
 
 void TrajectoryPlanner::calculateMotion() {
 	// Get combined kinematics
-	std::vector<std::pair<double, std::vector<double>>> combined_distance_kinematics = {};
+	std::vector<std::pair<double, std::vector<double>>> combined_distance_kinematics = _getCombinedKinematics();
 
-	// Convert to kinematics based on time
+	// Initialize result
 	time_kinematics = {};
+	double cumulativeTime = 0;
+
+	// Convert kinematics based on time
+	// t = Δv / a = 2d / (vi + vf)
+	const int segmentCount = (int) combined_distance_kinematics.size();
+	for (int segmentIndex = 0; segmentIndex < segmentCount; segmentIndex++) {
+		// Get kinematics info
+		auto &distance_kinematics = combined_distance_kinematics[segmentIndex];
+		const double d = distance_kinematics.first;
+		const double v = distance_kinematics.second[0];
+		const double u = (segmentIndex == segmentCount - 1) ? 0 : combined_distance_kinematics[segmentIndex + 1].second[0];
+		const double a = distance_kinematics.second[1];
+
+		// Push time kinematics
+		time_kinematics.push_back({cumulativeTime, {d, v, a}});
+		// printf("%.3f, %.3f, %.3f, %.3f\n", cumulativeTime, d, v, a);
+
+		// Get and update time
+		if (v + u == 0 && a == 0) {
+			printf("Error in trajectory!\n");
+			return;
+		}
+		const double time = (a != 0) ? (u - v) / a : 2 * d / (v + u);
+		cumulativeTime += time;
+	}
+
+	// Final zero time
+	time_kinematics.push_back({cumulativeTime, {totalDistance, 0, 0}});
 }
 
 std::vector<double> TrajectoryPlanner::getMotionAtTime(double time) {
+	// Validate stored motion
+	if (time_kinematics.empty()) {
+		return {0, 0, 0};
+	}
+
+	// Validate completion
+	if (time > time_kinematics.back().first) {
+		return time_kinematics.back().second;
+	}
+
 	// Binary search for the segment that contains the time
 	int bL, bR;
 	bL = 0;
@@ -227,9 +347,9 @@ std::vector<double> TrajectoryPlanner::getMotionAtTime(double time) {
 		double nodeTime = time_kinematics[bM].first;
 		if (nodeTime <= time) {
 			foundL = bM;
-			bM = bL + 1;
+			bL = bM + 1;
 		} else {
-			bM = bR - 1;
+			bR = bM - 1;
 		}
 	}
 
