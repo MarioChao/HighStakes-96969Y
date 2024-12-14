b5f0a011778f19939c7d130b676ef0323be22214
MarioChao <89794497+MarioChao@users.noreply.github.com>
2024-12-14 22:13:53 +0800

    red-down route plan

--- src/Autonomous/Paths/Official/redDown.cpp
+++ src/Autonomous/Paths/Official/redDown.cpp
@@ -18,15 +18,15 @@ void autonpaths::runAutonRedDown() {
 
 	// Set position and rotation
 	mainOdometry.printDebug();
-	mainOdometry.setPosition(0.68, 1.69);
-	setRobotRotation(105.0);
+	mainOdometry.setPosition(0.6, 0.35);
+	setRobotRotation(66.56);
 	mainOdometry.printDebug();
 
 	// Set config
 	setDifferentialUseRelativeRotation(true);
 
 	// Wait for arm reset
-	waitUntil(isArmResetted());
+	// waitUntil(isArmResetted());
 
 
 	/* Auton */
@@ -41,140 +41,100 @@ namespace {
 		clearSplines();
 
 		if (section == 1) {
-			// Rush goal
-			pushNewLinear({{2.62, 1.22}});
+			// Grab rushed goal
+			pushNewLinear({{2.2, 1.02}}, true, 60);
+
+			// Release goal
+			pushNewLinear({{1.99, 0.57}});
 
 			// Grab goal
-			pushNewLinear({{2.05, 1.97}}, true);
-
-			// Store 1 ring
-			pushNewLinear({{2, 1}});
-
-			// Grab goal
-			pushNewLinear({{2.62, 1.26}}, true);
+			pushNewLinear({{2, 2}}, true, 60);
 
 			// Go to corner
-			pushNewLinear({{1.04, 0.41}});
+			pushNewLinear({{0.43, 0.43}});
 
-			// Redirect corner ring
-			pushNewLinear({{0.25, 0.25}});
+			// Score 1 ring
+			pushNewLinear({{0.64, 0.64}}, true);
+			pushNewLinear({{0.64, 0}});
 
 			// Score alliance wall stake
-			pushNewLinear({{0.64, 3}, {0.3, 3}});
+			pushNewLinear({{0.55, 3.}}, true);
+			pushNewLinear({{0, 3}});
 
 			// Touch ladder
-			pushNewSpline(UniformCubicSpline::fromAutoTangent(cspline::CatmullRom, {
-				{0.21, 4.84}, {0.53, 2.98}, {2.45, 2.41}, {4.48, 3.95}
-			}), true);
+			pushNewLinear({{1.42, 2.39}, {2.45, 2.43}}, true);
 		}
 	}
 
 	void doAuton() {
 		/* Rush middle goal */
 
-		// Follow path
-		runFollowLinearYield();
+		// Store + rush
+		setIntakeStoreRing(1);
+		async_driveTurnToFace_tiles(2.15, 1.0);
+		// driveTurnToFace_tiles(2.15, 1.0, false, 80);
 
 		// Deploy
+		waitUntil(_linearPathDistanceError < 0.2);
 		setSwing2State(1);
+		setIntakeStoreRing(0);
+		setIntakeState(0);
 
 		// Go back
-		driveDistanceTiles(-0.5);
+		waitUntil(_isDriveTurnSettled);
+		driveDistanceTiles(-1.0);
+
+		// Un-deploy
 		setSwing2State(0);
 
+		// Grab goal
+		runFollowLinearYield();
+		setGoalClampState(1);
 
-		/* Grab goal*/
+		// Score stored
+		setIntakeState(1);
+		wait(200, msec);
 
 		// Follow path
 		runFollowLinearYield();
-
-		// Grab goal
-		setGoalClampState(1);
-		wait(200, msec);
-
-
-		/* Score 1 ring */
-
-		// Score preload
-		setIntakeState(1);
 
 		// Release goal
-		wait(1, sec);
 		setGoalClampState(0);
 
-
-		/* Store 1 ring */
-
-		// Start storing
-		setIntakeStoreRing(1, 0.3);
-
-		// Follow path
-		runFollowLinearYield();
-
-
-		/* Grab goal */
-
-		// Follow path
-		runFollowLinearYield();
-
 		// Grab goal
+		setIntakeState(0);
+		runFollowLinearYield();
 		setGoalClampState(1);
 		wait(200, msec);
 
-		// Start scoring
-		setIntakeState(1);
-
-
-		/* Sweep corner */
-
-		// Follow path
+		// Go to corner
 		runFollowLinearYield();
-
-		// Deploy swing
-		setSwingState(1);
-		// setIntakeState(0);
 
 		// Swing out rings
-		driveAndTurnDistanceTiles(0.7, -135, 60, 100, 0.5);
-		turnToAngleVelocity(-80, 50);
+		setSwingState(1);
+		wait(200, msec);
+		turnToAngleVelocity(160, 50);
 		setSwingState(0);
 
-
-		/* Redirect corner */
-
-		// Start redirect
+		// Score 1 ring
+		runFollowLinearYield();
 		setIntakeState(1);
-		setIntakeToArm(1);
-
-		// Follow path
 		runFollowLinearYield();
 
-		// Back up
-		driveAndTurnDistanceTiles(-0.5, -135, 100.0, 100.0, 1.0);
-
-
-		/* Score alliance wall stake */
-
-		// Raise arm
+		// Score alliance wall stake
 		setArmStage(2, 0.5);
-
-		// Follow path
 		runFollowLinearYield();
+		setIntakeState(0);
+		runFollowLinearYield();
+		driveDistanceTiles(-0.5);
 
-
-		/* Touch ladder */
-
-		// Lower arm
+		// Touch ladder
 		setArmStage(0, 1.0);
 
 		// Follow path
-		runFollowSpline();
+		runFollowLinearYield();
 
 		// Wait
 		waitUntil(_pathFollowCompleted);
-
-		// Stop intake
-		waitUntil(_autonTimer.value() > 14.5);
-		setIntakeState(0);
 	}
 }
