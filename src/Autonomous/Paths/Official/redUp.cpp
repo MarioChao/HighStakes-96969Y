9dc280a69e1e87e7c24ed29518d43d28de990e27
MarioChao <89794497+MarioChao@users.noreply.github.com>
2025-04-30 22:17:06 +0800

    auton: r-up take 9

--- src/Autonomous/Paths/Official/redUp.cpp
+++ src/Autonomous/Paths/Official/redUp.cpp
@@ -51,7 +51,7 @@ void doAuton() {
 	waitUntil(global::_driveToPointDistanceError < 0.15);
 	// Top ring
 	setIntakeState(1);
-	global::driveToPoint(robotChassis, global::driveToPoint_params(3_tiles, 4.8_tiles, 0.35_tiles, false, 40), true);
+	global::driveToPoint(robotChassis, global::driveToPoint_params(3_tiles, 4.8_tiles, 0.4_tiles, false, 40), true);
 	waitUntil(global::_driveToPointDistanceError < 0.2);
 	global::driveToPoint(robotChassis, global::driveToPoint_params(2.69_tiles, 6_tiles, 0.35_tiles, false, 60, 1.2), true);
 	waitUntil(global::_driveToPointDistanceError < 0.1);
