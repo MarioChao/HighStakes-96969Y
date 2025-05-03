8210edc548ec3f1fb3a0bab56528f9366687c810
MarioChao <89794497+MarioChao@users.noreply.github.com>
2025-05-03 17:33:20 +0800

    auton: b-down take 5

--- src/Autonomous/Paths/Official/blueDown.cpp
+++ src/Autonomous/Paths/Official/blueDown.cpp
@@ -72,7 +72,7 @@ void doAuton() {
 	wait(0.1, sec);
 	setIntakeStoreRing(true);
 	// Middle ring 1
-	global::driveToPoint(robotChassis, global::driveToPoint_params(6_tiles - (3_tiles), 3.05_tiles, 0.5_tiles, false, 60), true);
+	global::driveToPoint(robotChassis, global::driveToPoint_params(6_tiles - (3.05_tiles), 3.05_tiles, 0.5_tiles, false, 60), true);
 	waitUntil(global::_driveToPointDistanceError < 0.2);
 	setIntakeStoreRing(false);
 	setIntakeState(0, 0.1);
@@ -87,7 +87,7 @@ void doAuton() {
 	setIntakeStoreRing(true);
 	waitUntil(global::_driveToPointDistanceError < 0.2);
 	// Re-position swinged ring
-	global::turnToFace(robotChassis, global::turnToFace_params(6_tiles - (2.3_tiles), 1_tiles), true);
+	global::turnToFace(robotChassis, global::turnToFace_params(6_tiles - (2.5_tiles), 1_tiles), true);
 	waitUntil(global::_turnToFaceError_degrees < 10);
 	setIntakeStoreRing(false);
 	setIntakeState(1, 0.1);
